0e82df60f87c223e94f5d85304208a9a15a4efb3
cdsoftw <cdsoftworks@gmail.com>
2024-01-24 00:12:27 -0600

    Improve loadSite(); add users fixture

--- sample-app/cypress/support/siteHelper.js
+++ sample-app/cypress/support/siteHelper.js
@@ -1,7 +1,10 @@
+/* selectors */
+export const POST_FEED_ENTRY = '.post-excerpt'; // TODO: make less fragile
+
 /**
  * Load the app via the cypress baseUrl, waiting for posts to populate.
  */
 export function loadSite() {
   cy.visit('/');
-  cy.get('.posts-list').children().should('have.length.greaterThan', 3);
+  cy.get(POST_FEED_ENTRY).should('have.length.greaterThan', 1); // more than one post entry
 }
